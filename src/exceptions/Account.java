b661ef24e706d3cc92599b3d3a6c849ee80632fc
hotiaDiallo <ibrahimahotia.diallo@gmail.com>
2023-07-20 21:49:59 +0200

    Exceptions handling

---
+++ src/exceptions/Account.java
@@ -0,0 +1,33 @@
+package exceptions;
+
+public class Account {
+  private float balance;
+
+  public void deposit(float value)  {
+    if (value <= 0)
+      throw new IllegalArgumentException();
+    //doSomething()
+  }
+
+  public void withdraw(float value) throws AccountException {
+    if (value > balance)
+      throw new AccountException(new InsufficientFundsException());
+    //doSomething()
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
